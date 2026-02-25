9295bf0a980c2eded1b4a08876300ec63859c4a3
vlad_zdor_94@mail.ru <vlad_zdor_94@mail.ru>
2026-02-25 12:40:12 +0300

    lil bug fixes + feature: consumer now works only if env variable kafka.self-consumer=true passed

--- src/main/java/ru/koshkin/PerfTestHelper/Kafka/KafkaApplier.java
+++ src/main/java/ru/koshkin/PerfTestHelper/Kafka/KafkaApplier.java
@@ -1,18 +1,24 @@
 package ru.koshkin.PerfTestHelper.Kafka;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(
+        name = "kafka.self-consumer",
+        havingValue = "true",
+        matchIfMissing = false
+)
 public class KafkaApplier {
 
-    @KafkaListener(topics = KafkaTopic.AUTHENTIFICATION_TOPIC, groupId = "consumer-group")
+    @KafkaListener(topics = KafkaTopic.AUTHENTIFICATION_TOPIC, groupId = KafkaConsumerConfig.CONSUMER_GROUP_NAME)
     private void authTopicListener(String data) {
         System.out.println(data);
     }
 
 
-    @KafkaListener(topics = KafkaTopic.REGISTRATION_TOPIC, groupId = "consumer-group")
+    @KafkaListener(topics = KafkaTopic.REGISTRATION_TOPIC, groupId = KafkaConsumerConfig.CONSUMER_GROUP_NAME)
     private void regTopicListener(String data) {
         System.out.println(data);
     }
--- src/main/java/ru/koshkin/PerfTestHelper/Kafka/KafkaConsumerConfig.java
+++ src/main/java/ru/koshkin/PerfTestHelper/Kafka/KafkaConsumerConfig.java
@@ -1,8 +1,11 @@
 package ru.koshkin.PerfTestHelper.Kafka;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -13,17 +16,26 @@ import java.util.HashMap;
 
 @Configuration
 @EnableKafka
+@ConditionalOnProperty(
+        name = "kafka.self-consumer",
+        havingValue = "true",
+        matchIfMissing = false
+)
 public class KafkaConsumerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
 
+    public static final String CONSUMER_GROUP_NAME = "consumer-group";
+
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         var props = new HashMap<String, Object>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, CONSUMER_GROUP_NAME);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, OffsetResetStrategy.EARLIEST);
         return new DefaultKafkaConsumerFactory<>(props);
     }
 }
